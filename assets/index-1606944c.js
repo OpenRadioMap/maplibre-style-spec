b4a34079cc53f125fa12baa04b40df0ef74bcce7
dBitech <darcy@dbitech.ca>
2024-01-07 12:02:21 +0000

    Deploying to gh-pages from @ OpenRadioMap/maplibre-style-spec@86c48981510ec300c2a2b2a48429e2416b020b0d 🚀

---
+++ assets/index-1606944c.js
@@ -0,0 +1,16 @@
+import{g as c,a as o,i as r,c as s,t as f}from"./entry-client-75e337af.js";import{M as g}from"./markdown-cae447f5.js";import{s as d}from"./v8-0f602cae.js";import{I as h}from"./items-aacd8a7c.js";import"./property-67628b1b.js";import"./subtitle-e9b340a0.js";const u=f("<div><!#><!/><!#><!/>");function M(){const i=`# Root
+Root level properties of a MapLibre style specify the map's layers, tile sources and other resources, and default values for the initial camera position when not specified elsewhere.
+
+
+\`\`\`json
+{
+    "version": 8,
+    "name": "MapLibre Demo Tiles",
+    "sprite": "https://demotiles.maplibre.org/styles/osm-bright-gl-style/sprite",
+    "glyphs": "https://demotiles.maplibre.org/font/{fontstack}/{range}.pbf",
+    "sources": {... },
+    "layers": [...]
+}
+\`\`\`
+
+`;return(()=>{const e=c(u),n=e.firstChild,[t,a]=o(n.nextSibling),l=t.nextSibling,[p,m]=o(l.nextSibling);return r(e,s(g,{content:i}),t,a),r(e,s(h,{headingLevel:"2",get entry(){return d.$root}}),p,m),e})()}export{M as default};
